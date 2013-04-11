9d45c7dec8583d3f13a1655257076de5729a9c74
Mark Larter <mark.larter@outlook.com>
2013-04-11 10:04:24 -0600

    Tweak defaults.

--- jquery.ui.dateinput.js
+++ jquery.ui.dateinput.js
@@ -27,14 +27,12 @@
         defaults = {
             "showError": true,
             "errorClass": "errorInput",
-            "hasPicker": false,
+            "hasPicker": true,
             "hasButtons": false
-            "isDateRequired": false,
-            "hasTime": false,
-            "isTimeRequired": false,
-            "hasSeconds": false
+            "isRequired": false
         },
-		dateRegex = /^((([0]?[1-9]|1[0-2])(:|\.)[0-5][0-9]((:|\.)[0-5][0-9])?( )?(AM|am|aM|Am|PM|pm|pM|Pm))|(([0]?[0-9]|1[0-9]|2[0-3])(:|\.)[0-5][0-9]((:|\.)[0-5][0-9])?))$/;
+		//dateRegex = /^((([0]?[1-9]|1[0-2])(:|\.)[0-5][0-9]((:|\.)[0-5][0-9])?( )?(AM|am|aM|Am|PM|pm|pM|Pm))|(([0]?[0-9]|1[0-9]|2[0-3])(:|\.)[0-5][0-9]((:|\.)[0-5][0-9])?))$/;
+		dateRegex = /^[\s\S]*$/;
     
     // The plugin constructor.
     var dateInput = function(options, element) {
