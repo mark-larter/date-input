be7569bd2dbed5d792fa34043180b4033be83d1b
Mark Larter <mark.larter@outlook.com>
2013-04-12 08:52:23 -0600

    Add datepicker.onSelect handler to set dateInput value sooner.

--- jquery.ui.dateinput.js
+++ jquery.ui.dateinput.js
@@ -71,7 +71,11 @@
                     showCloseButton: hasButtons,
                     showNowButton: hasButtons,
                     showDeselectButton: hasButtons && !options.isRequired,
-                    onClose: function (date, inst) {
+					onSelect: function(date, inst) {
+                        var jqInst = $(this);
+                        jqInst.dateInput("setDate", date);
+					}
+                    onClose: function(date, inst) {
                         var jqInst = $(this);
                         jqInst.dateInput("setDate", date);
                         if (onComplete) { onComplete.apply(jqInst.dateInput, [self._dateValue]); }
